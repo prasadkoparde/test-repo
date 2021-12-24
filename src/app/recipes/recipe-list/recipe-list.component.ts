6b3d1326ef4b2d226600ad4fd9a2a57e6518a488
Prasad <pakoparde@gmail.com>
2021-12-24 19:00:20 +0530

    recipe models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+   new Recipe('A Test Recipe', 'This is simply a test','https://static01.nyt.com/images/2021/12/13/dining/MC-Classic-Potato-Latkes/merlin_199179672_13ff5d80-3b33-46de-9904-0ccaabe97917-articleLarge.jpg')
+  ];
+  constructor() {
 
-  constructor() { }
+   }
 
   ngOnInit() {
   }
