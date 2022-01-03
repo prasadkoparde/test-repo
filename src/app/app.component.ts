a9dcdd5b2e80d76084a00593b113ee5a1a4c3190
Prasad <pakoparde@gmail.com>
2022-01-03 19:02:09 +0530

    chapter 6 started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  featureClicked:string;
+  onSelectFeature(feature:string){
+    this.featureClicked=feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
+
 
 @Component({
     selector:'app-header',
@@ -6,5 +7,11 @@ import { Component } from "@angular/core";
     styleUrls:['./header.component.css']
 })
 export class HeaderComponent {
-   
+    @Output() selectedFeature:EventEmitter<string>=new EventEmitter<string>()
+    onRecipeSelect(feature:string){
+        this.selectedFeature.emit(feature)
+    }
+    onShoppingListSelect(feature:string){
+        this.selectedFeature.emit(feature)
+    }
 }
